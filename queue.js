8a4204d19de17bbd8dd0e87a10912f483bc5d236
OspreyFly <noahbengoff@gmail.com>
2024-07-25 08:07:16 -0500

    complete tests pass

--- queue.js
+++ queue.js
@@ -20,27 +20,49 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    const newNode = new Node(val);
+    if(!this.first) {
+      this.first = newNode;
+    }else{
+      this.last.next = newNode;
+    }
+    this.last = newNode;
+    this.size++;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if(!this.first) {
+      throw new Error("Can not dequeue from an empty queue");
+    }
+    const val = this.first.val;
+    this.first = this.first.next;
+    this.size--;
 
+    if(!this.first) {
+      this.last = null;
+    }
+
+    return val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    if(this.first){
+      return this.first.val;
+    }else{
+      throw new Error("Can not peek at null");
+    }
+    
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return !this.first; 
   }
 }
-
 module.exports = Queue;
--- stack.js
+++ stack.js
@@ -18,29 +18,60 @@ class Stack {
   }
 
   /** push(val): add new value to end of the stack. Returns undefined. */
+  // The "end" of the stack is the first node; like the first pancake put on a plate you get to it last
 
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.first) {
+      this.first = newNode; // If the stack is empty, the new node is both the first and the last
+      this.last = newNode;
+    } else {
+      newNode.next = this.first; // Add the new node to the end of the stack
+      if (this.first.next === null) {
+        this.last = this.first;
+      }
+      this.first = newNode; // Update the last pointer to the new node
+    }
+    this.size++; // Increment the size of the stack
   }
+  
+  
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
+  // The "top" of the stack is the last node; like the last pancake on top that gets most of the syrup
 
   pop() {
+    if(!this.first){
+      throw new Error("Can not remove node in empty stack");
+    }
 
+    const val = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    
+    if(this.size === 0) {
+      this.last = null;
+    }
+    return val;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    if (!this.first) {
+      throw new Error("Cannot peek at an empty stack");
+    }
+    return this.first.val;
   }
+  
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0 || !this.first && !this.last;
   }
+  
 }
 
 module.exports = Stack;
